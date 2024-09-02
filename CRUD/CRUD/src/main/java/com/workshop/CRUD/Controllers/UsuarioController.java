d56cfac4a090676ea5e995c64bfd2c9d1cb459fd
Lucas Oliveira <lucasantonio97@live.com>
2024-09-02 11:31:14 -0300

    Corrigindo erro ao cadastrar

--- CRUD/CRUD/src/main/java/com/workshop/CRUD/Controllers/UsuarioController.java
+++ CRUD/CRUD/src/main/java/com/workshop/CRUD/Controllers/UsuarioController.java
@@ -1,9 +1,14 @@
 package com.workshop.CRUD.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.workshop.CRUD.Entities.Usuarios;
 import com.workshop.CRUD.Services.UsuarioService;
 
 @RestController
@@ -12,5 +17,10 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService service;
+
+@PostMapping()
+public ResponseEntity<Usuarios> create(@RequestBody Usuarios Usuarios){
+    return new ResponseEntity<>(service.create(Usuarios), HttpStatus.CREATED);
+}
 
 }
